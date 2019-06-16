bc8db89ad40c6ca9bfe14b302850edcb5a9a332e
wxxaut <51811343+wxxaut@users.noreply.github.com>
2019-06-16 12:36:30 +0800

    Update test.c

--- test.c
+++ test.c
@@ -8,6 +8,6 @@ int main(int argc, const char *argv[])
 	printf("hello world...\n");
 	printf("hello world......\n");
 	printf("hello world.........\n");
-
+	printf("hello world.........hello\n");
 	return 0;
 }
